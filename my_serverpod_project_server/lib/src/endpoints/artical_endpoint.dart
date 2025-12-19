e82bb000e56a5e9e1477c7a932f0558aa4740680
deepakmewada20 <mewadadeepak367@gmail.com>
2025-12-19 22:25:44 +0530

    I create a Artical class and api class using auto gentret code

---
+++ my_serverpod_project_server/lib/src/endpoints/artical_endpoint.dart
@@ -0,0 +1,9 @@
+import 'package:serverpod/serverpod.dart';
+
+class ArticalEndpoint extends Endpoint {
+  //fetch all articles
+  Future<String> getArtical(Session session)async {
+    return 'articaal';
+  }
+}
+  // This class will handle
